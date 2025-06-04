d6ea9c398de2e47896f6dc28efe324f002c37927
bilalahmed <bilal.ahmed@10xengineers.ai>
2025-06-04 12:37:47 +0500

    Updated Bind file to cater FIFO and Router binds

--- bsg_noc_FV/bind/network_bind.sv
+++ bsg_noc_FV/bind/network_bind.sv
@@ -9,14 +9,24 @@ bind design_top mesh4x4_sva #(
 )
 U_mesh_sva (.*);
 
-/*
-bind design_top network_sva #(
-  .DATA_WIDTH_P(DATA_WIDTH_P),
-  .MESH_EDGE_P (MESH_EDGE_P),
-  .medge_lp(medge_lp), 
-  .msize_lp(msize_lp),
-  .dims_lp (dims_lp),
-  .dirs_lp (dirs_lp)
+
+bind bsg_mesh_router router_sva #(
+  .width_p(12),
+  .x_cord_width_p(2),
+  .y_cord_width_p(2)
 )
-U_mesh_sva (.*); 
-*/
+U_router_sva (.*);
+
+//
+//bind bsg_mesh_router router_sva #(
+//  .width_p(12),
+//  .x_cord_width_p(2),
+//  .y_cord_width_p(2)
+//)
+//U_router_sva (.*);
+
+
+bind bsg_two_fifo fifo_sva #(.ptr_width_lp(1)) // fifo 2 has depth 2^1
+fifo_asserts(.*);
+
+
