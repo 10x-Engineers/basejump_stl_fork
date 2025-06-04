ddc5b6ab55bd79347acc0be86264692d7ec654fc
bilalahmed <bilal.ahmed@10xengineers.ai>
2025-06-04 12:41:08 +0500

    Updated tcl script to cater all single links and 0 to 15 link

--- bsg_noc_FV/scripts/jg.tcl
+++ bsg_noc_FV/scripts/jg.tcl
@@ -8,8 +8,9 @@ clear -all
 analyze -sv09 -f /work/bahmed/noc_verif/bsg_noc_org/flist.f
 
 analyze -sv09 /work/bahmed/noc_verif/bsg_noc_org/bind/network_bind.sv \
-			  /work/bahmed/noc_verif/bsg_noc_org/sva/mesh4x4.sv
-#			  /work/bahmed/noc_verif/bsg_noc_org/sva/mesh2x2.sv
+			  /work/bahmed/noc_verif/bsg_noc_org/sva/mesh_east_south.sv \
+			  /work/bahmed/noc_verif/bsg_noc_org/sva/fifo_sva.sv\
+			  /work/bahmed/noc_verif/bsg_noc_org/sva/router_sva.sv
 
 # check_cov -init -type all -model all
 
@@ -35,42 +36,92 @@ set_proofgrid_manager on
 #prove -covers -all
 
 
-#all assertions
-#prove -property {design_top(\\.[^.]+)*\\.[a-zA-Z0-9_]*} -regexp
+set_proofmaster on
 
 
 # Proving all Router Properties
-prove -property {design_top\.mesh_rou*}
+prove -property {design_top\.mesh_routers\[5]*}
 
 # Assuming all Router Properties
-assume -from_assert "design_top\.mesh_rou*"
+assume -from_assert "design_top\.mesh_routers\[5\]*"
 
+set_log_timestamp_mode on
+
+set_proofmaster on
+set_proofmaster_dir /work/bahmed/noc_verif/common/bsg_noc_org/
 
 
 # Proving all Fifo Properties
-prove -property {design_top\.fifos*}
+prove -property {design_top\.fifos_vertical\[0\]\.fifo_up*}
 
 # Assuming all FIFO Properties
-assume -from_assert "design_top\.fifos*"
+assume -from_assert "design_top\.fifos_vertical\[0\]\.fifo_up*"
 
 
-prove -property {design_top.U_mesh_sva.watch_pkt[0].genblk5.data_integ0_3}
-prove -property {design_top.U_mesh_sva.watch_pkt[1].genblk5.data_integ0_3}
-prove -property {design_top.U_mesh_sva.watch_pkt[2].genblk5.data_integ0_3}
-prove -property {design_top.U_mesh_sva.watch_pkt[3].genblk6.data_integ3_7} 
-prove -property {design_top.U_mesh_sva.watch_pkt[4].genblk7.data_integ4_7}
-prove -property {design_top.U_mesh_sva.watch_pkt[5].genblk7.data_integ4_7}
-prove -property {design_top.U_mesh_sva.watch_pkt[6].genblk7.data_integ4_7}
+prove -property {design_top.U_mesh_sva.watch_pkt[0].genblk6.east.data_integ0_3_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[0].genblk6.east.data_integ0_3_east}
+prove -property {design_top.U_mesh_sva.watch_pkt[1].genblk6.east.data_integ0_3_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[1].genblk6.east.data_integ0_3_east}
+prove -property {design_top.U_mesh_sva.watch_pkt[2].genblk6.east.data_integ0_3_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[2].genblk6.east.data_integ0_3_east}
 
-assume -from_assert  {design_top.U_mesh_sva.watch_pkt[0].genblk5.data_integ0_3}
-assume -from_assert  {design_top.U_mesh_sva.watch_pkt[1].genblk5.data_integ0_3}
-assume -from_assert  {design_top.U_mesh_sva.watch_pkt[2].genblk5.data_integ0_3}
-assume -from_assert  {design_top.U_mesh_sva.watch_pkt[3].genblk6.data_integ3_7}
-assume -from_assert  {design_top.U_mesh_sva.watch_pkt[4].genblk7.data_integ4_7}
-assume -from_assert  {design_top.U_mesh_sva.watch_pkt[5].genblk7.data_integ4_7}
-assume -from_assert  {design_top.U_mesh_sva.watch_pkt[6].genblk7.data_integ4_7}
 
-prove -property {design_top.U_mesh_sva.watch_pkt[8].genblk8.data_integ0_7}  -time_limit {72h}
 
+
+prove -property {design_top.U_mesh_sva.watch_pkt[0].genblk6.data_integ0_3_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[0].genblk6.data_integ0_3_south}
+prove -property {design_top.U_mesh_sva.watch_pkt[1].genblk6.data_integ0_3_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[1].genblk6.data_integ0_3_south}
+prove -property {design_top.U_mesh_sva.watch_pkt[2].genblk6.data_integ0_3_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[2].genblk6.data_integ0_3_south}
+prove -property {design_top.U_mesh_sva.watch_pkt[3].genblk6.data_integ0_3_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[3].genblk6.data_integ0_3_south}
+
+prove -property {design_top.U_mesh_sva.watch_pkt[4].genblk7.east.data_integ3_7_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[4].genblk7.east.data_integ3_7_east}
+prove -property {design_top.U_mesh_sva.watch_pkt[5].genblk7.east.data_integ3_7_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[5].genblk7.east.data_integ3_7_east}
+prove -property {design_top.U_mesh_sva.watch_pkt[6].genblk7.east.data_integ3_7_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[6].genblk7.east.data_integ3_7_east}
+
+
+
+
+prove -property {design_top.U_mesh_sva.watch_pkt[4].genblk7.data_integ3_7_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[4].genblk7.data_integ3_7_south}
+prove -property {design_top.U_mesh_sva.watch_pkt[5].genblk7.data_integ3_7_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[5].genblk7.data_integ3_7_south}
+prove -property {design_top.U_mesh_sva.watch_pkt[6].genblk7.data_integ3_7_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[6].genblk7.data_integ3_7_south}
+prove -property {design_top.U_mesh_sva.watch_pkt[7].genblk7.data_integ3_7_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[7].genblk7.data_integ3_7_south}
+
+
+prove -property {design_top.U_mesh_sva.watch_pkt[8].genblk8.east.data_integ7_11_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[8].genblk8.east.data_integ7_11_east}
+prove -property {design_top.U_mesh_sva.watch_pkt[9].genblk8.east.data_integ7_11_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[9].genblk8.east.data_integ7_11_east}
+prove -property {design_top.U_mesh_sva.watch_pkt[10].genblk8.east.data_integ7_11_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[10].genblk8.east.data_integ7_11_east}
+
+
+
+prove -property {design_top.U_mesh_sva.watch_pkt[8].genblk8.data_integ7_11_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[8].genblk8.data_integ7_11_south}
+prove -property {design_top.U_mesh_sva.watch_pkt[9].genblk8.data_integ7_11_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[9].genblk8.data_integ7_11_south}
+prove -property {design_top.U_mesh_sva.watch_pkt[10].genblk8.data_integ7_11_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[10].genblk8.data_integ7_11_south}
+prove -property {design_top.U_mesh_sva.watch_pkt[11].genblk8.data_integ7_11_south}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[11].genblk8.data_integ7_11_south}
+
+prove -property {design_top.U_mesh_sva.watch_pkt[12].east.data_integ11_15_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[12].east.data_integ11_15_east}
+prove -property {design_top.U_mesh_sva.watch_pkt[13].east.data_integ11_15_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[13].east.data_integ11_15_east}
+prove -property {design_top.U_mesh_sva.watch_pkt[14].east.data_integ11_15_east}
+assume -from_assert  {design_top.U_mesh_sva.watch_pkt[14].east.data_integ11_15_east}
+
+prove -property {design_top.U_mesh_sva.watch_pkt[15].genblk10.data_integ0_15}  -time_limit {110h} 
 
 #prove -bg -all 
